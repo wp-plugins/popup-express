f048e2241ff72f1bd9e6ec803557671d71825af1
jeffbullins <jeffbullins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-23 21:34:53 +0000

    Updated version number to 1.01
    git-svn-id: https://plugins.svn.wordpress.org/popup-express/tags/1.01@880597 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- popup-express.php
+++ popup-express.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Popup Express
 Description: An easy way to create a popup lightbox with an email form to collect email addresses.
-Version: 1.0
+Version: 1.01
 Author: Jeff Bulllins
 Author URI: http://www.thinklandingpages.com
 */
